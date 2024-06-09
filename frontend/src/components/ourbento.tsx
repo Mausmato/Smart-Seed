38d9f4e0c187de0870061452781ae413d232f8c3
Mausmato <Singeripad2@gmail.com>
2024-06-09 01:32:08 -0400

    nice

--- frontend/src/components/ourbento.tsx
+++ frontend/src/components/ourbento.tsx
@@ -1,17 +1,23 @@
-import { cn } from "../../lib/utils";
 import React from "react";
-
-import "../../src/index.css";
-
 import { BentoGrid, BentoGridItem } from "../components/bento";
 
-export const CustomBentoGrid = () => {
+interface CustomBentoGridProps {
+  predictionData: PredictionData;
+}
+
+export const CustomBentoGrid: React.FC<CustomBentoGridProps> = ({
+  predictionData,
+}) => {
   return (
     <BentoGrid className="p-8 h-screen relative">
       <div className="w-full">
         <BentoGridItem
           className="bg-custom-gradient text-white w-full"
-          title={<div className="text-5xl">240%</div>}
+          title={
+            <div className="text-5xl">
+              {predictionData.class_probabilities["Moisture Levels"] ?? "N/A"}
+            </div>
+          }
           description={<div>Moisture Levels</div>}
           icon={<div className="icon-placeholder" />}
         />
@@ -19,20 +25,32 @@ export const CustomBentoGrid = () => {
       <div className="w-full h-[148.797px] grid grid-cols-2 gap-4">
         <BentoGridItem
           className="bg-white text-black w-full h-[200.797px]"
-          title={<div className="text-5xl  text-neutral-900">99%</div>}
+          title={
+            <div className="text-5xl  text-neutral-900">
+              {predictionData.class_probabilities["Nutrient 1"] ?? "N/A"}
+            </div>
+          }
           description={<div className=" text-neutral-800">Nutrient 1</div>}
           icon={<div className="icon-placeholder" />}
         />
         <BentoGridItem
           className="bg-white text-black w-full h-[200.797px]"
-          title={<div className="text-5xl text-neutral-900">32%</div>}
+          title={
+            <div className="text-5xl text-neutral-900">
+              {predictionData.class_probabilities["Nutrient 2"] ?? "N/A"}
+            </div>
+          }
           description={<div className=" text-neutral-800">Nutrient 2</div>}
           icon={<div className="icon-placeholder" />}
         />
         <div className="col-span-2 h-[225.797px]">
           <BentoGridItem
             className="bg-white text-black w-full h-[234px]"
-            title={<div className="text-5xl text-neutral-900">60%</div>}
+            title={
+              <div className="text-5xl text-neutral-900">
+                {predictionData.class_probabilities["Nutrient 3"] ?? "N/A"}
+              </div>
+            }
             description={<div className=" text-neutral-800">Nutrient 3</div>}
             icon={<div className="icon-placeholder" />}
           />
